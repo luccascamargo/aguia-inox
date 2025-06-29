40aa019cfb53f089f9e25571e4b5ba2bcc4ae7a6
Lucas Camargo <lucascamargo.dev@gmail.com>
2025-06-29 19:30:38 -0300

    Pagina empresa

---
+++ resources/js/lib/mocks.ts
@@ -0,0 +1,46 @@
+export const diferenciais = [
+  {
+    title: '100% Aço inox',
+    description: 'Nossos produtos levam o selo de confiança e durabilidade do aço inoxidável.',
+  },
+  {
+    title: 'Tecnologia europeia',
+    description: 'Nossos maquinários são provenientes dos principais polos industriais europeus.',
+  },
+  {
+    title: 'Projetos personalizados',
+    description: 'Nossas equipes de engenharia possuem know-how para discutir variados processos produtivos.',
+  },
+  {
+    title: 'Aprovada por multinacionais',
+    description: 'Continuamente recebemos visitas de inspetores que acompanham os processos fabris.',
+  },
+  {
+    title: 'Exportação para diversos países',
+    description: 'Estabelecemos parcerias especializadas para garantir as documentações, logística e trâmites internacionais.',
+  },
+  {
+    title: 'Qualidade que monitora processos',
+    description: 'Nosso departamento de Qualidade confere e inspeciona diariamente os processos fabris.',
+  },
+  {
+    title: 'Treinamentos e certificações constantes',
+    description: 'Especialização periódica para elevar o nível de conhecimento e as competências da nossa mão de obra direta.',
+  },
+  {
+    title: 'Equipe externa especializada',
+    description: 'Realizamos os serviços de instalações, manutenções, assistências técnicas ou start em equipamentos com a nossa equipe diretamente.',
+  },
+  {
+    title: 'Programação de matéria-prima',
+    description: 'Compras periódicas antecipadas de matéria-prima garantem a segurança nos investimentos dos nossos clientes.',
+  },
+  {
+    title: 'Logística especializada',
+    description: 'Nosso serviço de transporte e acompanhamento de carga é especialista em cargas especiais, complexas ou de grande porte.',
+  },
+  {
+    title: 'Eficiência em prazos de entrega',
+    description: 'Nosso report semanal de pós-venda já é um diferencial consolidado e reconhecido pelos nossos clientes.',
+  },
+];
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100;200;300;400;500;600;700&family=Montserrat:wght@100:200:300;400;500;600;700&display=swap" rel="stylesheet">
 
         @routes
         @viteReactRefresh
